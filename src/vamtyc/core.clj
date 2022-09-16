e0d4ace3034fd9ed31ba92ad6922012cc8eb515c
guillerglez88 <guillerglez88@gmail.com>
2022-09-16 20:07:11 +0200

    ability to RDD

--- src/vamtyc/core.clj
+++ src/vamtyc/core.clj
@@ -4,19 +4,33 @@
 (defn respond-hello
   [request]
   {:status 200
-   :body "Hello World"})
+   :body "Hello World!"})
 
 (def routes
   #{["/hello" :get `respond-hello]})
 
-(defn server
+(defn start
   []
   (-> {::http/routes routes
        ::http/port 8890
        ::http/type :jetty
-       ::http/host "0.0.0.0"}
+       ::http/host "0.0.0.0"
+       ::http/join? false}
       http/create-server
       http/start))
 
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server (start)))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
 (comment
-  (server))
+  (start-dev)
+  (restart))
