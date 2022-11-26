5cc580437be2d5cdf7e10ba0056cfd3b038d3fa5
guillerglez88 <guillerglez88@gmail.com>
2022-11-26 16:11:39 +0100

    refactoring build-route

--- src/vamtyc/resources.clj
+++ src/vamtyc/resources.clj
@@ -3,60 +3,23 @@
             [vamtyc.data.schema :as schema]
             [vamtyc.data.store :as store]))
 
-(defn list [resourceType]
+(defn build-route [name-prefix method resourceType type]
   (let [res-name    (name resourceType)
-        route-name  (str "list-" (str/lower-case res-name))]
-    {:method  "GET"
+        route-name  (str name-prefix "-" (str/lower-case res-name))
+        resource    (str "/Resource/" res-name)]
+    {:method  method
      :path    [{:name "resourceType" :value res-name}]
      :name    route-name
+     :resource resource
      :type    "core"
      :handler "list"}))
 
-(defn read [resourceType]
-  (let [res-name    (name resourceType)
-        route-name  (str "read-" (str/lower-case res-name))]
-    {:method  "GET"
-     :path    [{:name "resourceType" :value res-name}
-               {:name "id"}]
-     :name    route-name
-     :type    "core"
-     :handler "read"}))
-
-(defn create [resourceType]
-  (let [res-name    (name resourceType)
-        route-name  (str "create-" (str/lower-case res-name))]
-    {:method  "POST"
-     :path    [{:name "resourceType" :value res-name}]
-     :name    route-name
-     :type    "core"
-     :handler "create"}))
-
-(defn upsert [resourceType]
-  (let [res-name    (name resourceType)
-        route-name  (str "upsert-" (str/lower-case res-name))]
-    {:method  "POST"
-     :path    [{:name "resourceType" :value res-name}
-               {:name "id"}]
-     :name    route-name
-     :type    "core"
-     :handler "upsert"}))
-
-(defn delete [resourceType]
-  (let [res-name    (name resourceType)
-        route-name  (str "delete-" (str/lower-case res-name))]
-    {:method  "POST"
-     :path    [{:name "resourceType" :value res-name}
-               {:name "id"}]
-     :name    route-name
-     :type    "core"
-     :handler "delete"}))
-
 (defn build-routes [resourceType]
-  [(list    resourceType)
-   (read    resourceType)
-   (create  resourceType)
-   (upsert  resourceType)
-   (delete  resourceType)])
+  [(build-route "list"      :GET    resourceType    :core)
+   (build-route "read"      :GET    resourceType    :core)
+   (build-route "create"    :POST   resourceType    :core)
+   (build-route "upsert"    :PUT    resourceType    :core)
+   (build-route "delete"    :DELETE resourceType    :core)])
 
 (defn provision [resourceType]
   (let [routes (build-routes resourceType)]
@@ -69,4 +32,5 @@
   (provision :HttpRoute)
   (provision :Currency)
   (provision :MoneyExchange)
+  (provision :Resource)
   )
