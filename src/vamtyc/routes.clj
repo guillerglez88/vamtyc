a78a52973687e6c502c3943b6cd0d41434c63e4a
guillerglez88 <guillerglez88@gmail.com>
2022-11-27 11:16:25 +0100

    extracting path utils

--- src/vamtyc/routes.clj
+++ src/vamtyc/routes.clj
@@ -2,25 +2,23 @@
   (:require [clojure.data.json :as json]
             [clojure.string :as str]
             [compojure.core :refer [make-route routes]]
-            [vamtyc.data.store :as store]))
+            [vamtyc.data.store :as store]
+            [vamtyc.utils.path :as path]))
 
 (defn meta-handler [route]
   (fn [req]
     (let [parsed-req    (select-keys req [:uri :params :form-params :query-params])
           body          (merge parsed-req {:route route})]
-        {:status 200
-        :headers {"Content-Type" "application/json"}
-        :body (json/write-str body)})))
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body (json/write-str body)})))
 
-(defn build-cpj-path [path]
-  (->> path
-       (map (fn [cmp] (or (:value cmp) (str ":" (:name cmp)))))
-       (str/join "/")
-       (str "/")))
+;; (def core-handlers
+;;   {:core/})
 
 (defn build-cpj-route [route]
   (let [method  (-> route :method str/lower-case keyword)
-        path    (-> route :path build-cpj-path)
+        path    (-> route :path path/stringify)
         handler (meta-handler route)]
     (make-route method path handler)))
 
---
+++ src/vamtyc/utils/path.clj
@@ -0,0 +1,30 @@
+(ns vamtyc.utils.path)
+
+(defn is-res-type? [path-cmp]
+  (-> path-cmp :name (= "resourceType")))
+
+(defn get-res-type [path]
+  (let [path-cmp (filter is-res-type? path)]
+    (-> path-cmp first :value)))
+
+(defn stringify [path]
+  (->> path
+       (map (fn [cmp] (or (:value cmp) (str ":" (:name cmp)))))
+       (str/join "/")
+       (str "/")))
+
+(comment
+  (is-res-type? {:name "resourceType"})
+  (is-res-type? {:name "id"})
+  ;;
+  (get-res-type [{:name "resourceType" :value "Resource"}
+                 {:name "id"}])
+  (get-res-type [{:name "tenant"        :value "instance1"}
+                 {:name "resourceType"  :value "Resource"}
+                 {:name "id"}])
+  ;;
+  (stringify [{:name "resourceType"  :value "Resource"}])
+  (stringify [{:name "tenant"        :value "instance1"}
+              {:name "resourceType"  :value "Resource"}
+              {:name "id"}])
+  )
