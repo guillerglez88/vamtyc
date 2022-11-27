dbb58e6116c4102f8fbaf6d4f34278c541edd384
guillerglez88 <guillerglez88@gmail.com>
2022-11-27 22:14:04 +0100

    refactoring modules initialization

--- src/vamtyc/resources.clj
+++ src/vamtyc/resources.clj
@@ -1,50 +1,14 @@
 (ns vamtyc.resources
-  (:require [clojure.string :as str]
-            [vamtyc.data.schema :as schema]
-            [vamtyc.data.store :as store]
-            [vamtyc.utils.path :as path]))
+  (:require [vamtyc.data.schema :as schema]
+            [vamtyc.data.store :as store]))
 
-(defn build-route [code method path]
-  (let [res-type    (path/get-res-type path)
-        res-name    (name res-type)
-        res-name-lc (str/lower-case res-name)
-        route-name  (str code "-" res-name-lc)
-        resource    (str "/Resource/" res-name-lc)
-        coding      (str "/Coding/core-handlers?code=" code)]
-    {:method    method
-     :path      path
-     :name      route-name
-     :resource  resource
-     :type      :core
-     :code      coding}))
-
-(defn build-routes [resourceType]
-  (let [res-type    {:name "resourceType" :value resourceType}
-        id          {:name "id"}]
-    [(build-route "list"      :GET    [res-type   ])
-     (build-route "read"      :GET    [res-type id])
-     (build-route "create"    :POST   [res-type   ])
-     (build-route "upsert"    :PUT    [res-type id])
-     (build-route "delete"    :DELETE [res-type id])]))
-
-(def base-entities
-  [{:id     "resource"
-    :type   :Resource
-    :desc   "Represents a REST resource"}
-   {:id     "route"
-    :type   :Route
-    :desc   "Represents a REST route"}])
-
-(defn bootstrap []
-  (for [resource    base-entities
-        :let        [resourceType   (:type  resource)
-                     id             (:id    resource)]]
-    (do
-      (schema/provision resourceType)
-      (store/create :Resource id resource)
-      (for [route (build-routes resourceType)]
-        (store/create :Route route)))))
+(defn init []
+  (let [resource    {:type :Resource
+                     :desc "Represents a REST resource"}
+        id          "resource"]
+    (schema/provision :Resource)
+    (store/create :Resource id resource)))
 
 (comment
-  (bootstrap)
+  (init)
   )
--- src/vamtyc/routes.clj
+++ src/vamtyc/routes.clj
@@ -3,11 +3,35 @@
             [clojure.string         :as     str]
             [compojure.core         :refer  [make-route routes]]
             [lambdaisland.uri       :refer  [uri query-string->map]]
+            [vamtyc.data.schema     :as     schema]
             [vamtyc.data.store      :as     store]
             [vamtyc.utils.path      :as     path]
             [vamtyc.handlers.list   :as     list]
             [vamtyc.handlers.read   :as     read]
             [vamtyc.handlers.create :as     create]))
+
+(defn build-route [code method path]
+  (let [res-type    (path/get-res-type path)
+        res-name    (name res-type)
+        res-name-lc (str/lower-case res-name)
+        route-name  (str code "-" res-name-lc)
+        resource    (str "/Resource/" res-name-lc)
+        coding      (str "/Coding/core-handlers?code=" code)]
+    {:method    method
+     :path      path
+     :name      route-name
+     :resource  resource
+     :type      :core
+     :code      coding}))
+
+(defn build-routes [resourceType]
+  (let [res-type    {:name "resourceType" :value resourceType}
+        id          {:name "id"}]
+    [(build-route "list"      :GET    [res-type   ])
+     (build-route "read"      :GET    [res-type id])
+     (build-route "create"    :POST   [res-type   ])
+     (build-route "upsert"    :PUT    [res-type id])
+     (build-route "delete"    :DELETE [res-type id])]))
 
 (defn meta-handler [route]
   (fn [req]
@@ -30,12 +54,23 @@
         handler (-> hkey handlers (or meta-handler) (#(% route)))]
     (make-route method path handler)))
 
+(defn init []
+  (let [resource  {:type :Route
+                   :desc "Represents a REST route"}
+        id         "route"]
+    (schema/provision :Route)
+    (store/create :Resource id resource)
+    (for [resource  (store/list :Resource)
+          route     (-> resource :type keyword build-routes)]
+      (store/create :Route route))))
+
 (defn load-cpj-routes []
   (->> (store/list :Route)
        (map build-cpj-route)
        (apply routes)))
 
 (comment
+  (init)
   (load-cpj-routes)
   (uri "/Coding/core-handlers?code=list")
   )
