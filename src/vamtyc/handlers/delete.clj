ac8a9adcbdc9ed939d758a9e5914dcb8180fe0e6
guillerglez88 <guillerglez88@gmail.com>
2022-11-28 20:37:57 +0100

    delete when res not found

--- src/vamtyc/handlers/delete.clj
+++ src/vamtyc/handlers/delete.clj
@@ -1,14 +1,16 @@
 (ns vamtyc.handlers.delete
-  (:require [ring.util.response :refer  [content-type response status]]
+  (:require [ring.util.response :refer  [content-type response status not-found]]
             [vamtyc.data.store  :as     store]
             [vamtyc.utils.path  :as     path]
             [clojure.data.json  :as     json]))
 
 (defn handler [req route]
   (let [res-type  (-> route :path path/get-res-type keyword)
-        id        (-> req :params :id)]
-    (-> res-type
-        (store/delete id)
-        (json/write-str)
-        (response)
-        (content-type "application/json"))))
+        id        (-> req :params :id)
+        res       (store/delete res-type id)]
+    (if res
+      (-> res
+          (json/write-str)
+          (response)
+          (content-type "application/json"))
+      (not-found "Not found"))))
