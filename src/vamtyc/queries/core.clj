4b074ce05e843886b2aae2040bce89247443903a
guillerglez88 <guillerglez88@gmail.com>
2022-12-06 18:48:50 +0100

    cleaning queries/core module :require

--- src/vamtyc/queries/core.clj
+++ src/vamtyc/queries/core.clj
@@ -1,10 +1,9 @@
 (ns vamtyc.queries.core
-  (:require [vamtyc.data.store :as store]
-            [honey.sql :as hsql]
-            [honey.sql.helpers :refer [select from where] :as h]
+  (:require [honey.sql :as sql]
             [vamtyc.queries.limit :as limit]
             [vamtyc.utils.requests :refer [extract-param-names]]
-            [vamtyc.utils.queries :refer [make-sql-map]]
+            [vamtyc.utils.queries :refer [make-sql-map make-prop-alias
+                                          jsonb-extract-prop jsonb-extract-coll]]
             [vamtyc.data.queryparams :refer [load-queryparams]]
             [vamtyc.queries.text :as text]))
 
@@ -13,8 +12,18 @@
    :/Coding/core-query-params?code=text   text/filter})
 
 (defn refine-query [req sql-map query-param]
-  (-> query-param :code keyword filters
-      (#(% req query-param sql-map))))
+  (let [filter (-> query-param :code keyword filters)]
+    (loop [acc            sql-map
+           col            :resource
+           [curr & rest]  (:path query-param)]
+      (cond
+        (nil? curr)         (filter req query-param acc col)
+        (:collection curr)  (let [alias (make-prop-alias col curr "_elem")]
+                              (-> (jsonb-extract-coll acc col curr alias)
+                                  (recur alias rest)))
+        :else               (let [alias (make-prop-alias col curr)]
+                              (-> (jsonb-extract-prop acc col curr alias)
+                                  (recur alias rest)))))))
 
 (defn process-query-params [req tx]
   (let [res-type    (-> req :body :resourceType)
@@ -22,7 +31,7 @@
         queryparams (load-queryparams param-names res-type tx)
         sql-map     (make-sql-map res-type)]
     (if (empty? queryparams)
-      (assoc req :sql (hsql/format sql-map {:pretty true}))
+      (assoc req :sql (sql/format sql-map {:pretty true}))
       (-> (reduce #(refine-query req %1 %2) sql-map queryparams)
-          (hsql/format {:pretty true})
+          (sql/format {:pretty true})
           (->> (assoc req :queryparams queryparams :sql))))))
--- src/vamtyc/queries/text.clj
+++ src/vamtyc/queries/text.clj
@@ -4,17 +4,7 @@
                                           jsonb-extract-coll
                                           jsonb-extract-prop]]))
 
-(defn filter [req query-param sql-map]
+(defn filter [req query-param sql-map col]
   (let [name  (:name query-param)
         val   (-> req :params (get name))]
-    (loop [acc              sql-map
-           jsonb            :resource
-           [curr & rest]    (:path query-param)]
-      (cond
-        (nil? curr)         (where acc [:like [:cast jsonb :text] (str "%" val "%")])
-        (:collection curr)  (let [alias (make-prop-alias jsonb curr "_elem")]
-                              (-> (jsonb-extract-coll acc jsonb curr alias)
-                                  (recur alias rest)))
-        :else               (let [alias (make-prop-alias jsonb curr)]
-                              (-> (jsonb-extract-prop acc jsonb curr alias)
-                                  (recur alias rest)))))))
+    (where sql-map [:like [:cast col :text] (str "%" val "%")])))
