5a8834726c319727124585d2155a43e008270f10
guillerglez88 <guillerglez88@gmail.com>
2022-12-07 22:32:16 +0100

    selecting fields for result, first approach

--- src/vamtyc/utils/cpjroutes.clj
+++ src/vamtyc/utils/cpjroutes.clj
@@ -32,6 +32,37 @@
         route-code      (-> route :code keyword)]
     (if is-inspect inspect-code route-code)))
 
+(defn flat-fields [fields-param]
+  (cond
+    (string? fields-param)  (->> (str/split fields-param #",")
+                                 (map #(str/split % #"\."))
+                                 (into []))
+    (vector? fields-param)  (->> (mapcat flat-fields fields-param)
+                                 (into []))
+    :else                   []))
+
+(defn select-path-into [path from to]
+  (let [[field-str & rest]  path
+        field               (keyword field-str)
+        from-prop           (field from)]
+    (cond
+      (empty? rest)       (assoc to field from-prop)
+      (vector? from-prop) (->> (or (field to) (map (fn [_] (hash-map)) from-prop))
+                               (map #(select-path-into rest %1 %2) from-prop)
+                               (into [])
+                               (assoc to field))
+      :else               (->> (field to)
+                               (or {})
+                               (select-path-into rest from-prop)
+                               (assoc to field)))))
+
+(defn select-fields [resp params]
+  (let [fields (-> params (get "_fields") (or []) (flat-fields))]
+    (if (empty? fields) resp
+        (->> (reduce #(select-path-into %2 (:body resp) %1) {} fields)
+             (hash-map :body)
+             (merge resp)))))
+
 (defn make-http-response [resp]
   (-> (json/write-str (:body resp))
       (response)
@@ -45,6 +76,7 @@
       (-> (requests/build-request req route)
           (queries/process-query-params tx)
           (handler tx)
+          (select-fields (:params req))
           (make-http-response)))))
 
 (defn build-cpj-route [route]
