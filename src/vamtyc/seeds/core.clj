918abd0b2b7cc8d030a685cd0a0a7b88bb62f470
guillerglez88 <guillerglez88@gmail.com>
2022-12-08 22:39:18 +0100

    seed runner

---
+++ src/vamtyc/seeds/core.clj
@@ -0,0 +1,51 @@
+(ns vamtyc.seeds.core
+  (:require [clojure.edn :as edn]
+            [vamtyc.data.store :as store]
+            [vamtyc.data.datasource :refer [ds]]
+            [next.jdbc :as jdbc]
+            [clojure.string :as str]))
+
+(defn is-already-init? []
+  (try
+    (store/list ds :Resource)
+    true
+    (catch Exception _ false)))
+
+(defn make-storage-ddl [type]
+  (let [type-name (-> type name str/lower-case)]
+    [(str "CREATE TABLE IF NOT EXISTS public." type-name " (
+       id          TEXT    NOT NULL,
+       resource    JSONB   NULL,
+       CONSTRAINT  " type-name "_pk PRIMARY KEY (id));")]))
+
+(defn allocate-storage [tx type]
+  (->> (make-storage-ddl type)
+       (jdbc/execute! tx)))
+
+(defn commit-boot-trn-item [tx res]
+  (let [res-type    (:resourceType res)
+        id          (:id res)
+        type        (:type res)]
+    (when (= :Resource res-type)
+      (allocate-storage tx type))
+    (if (nil? id)
+      (store/create tx res-type res)
+      (store/create tx res-type id res))))
+
+(defn commit-boot-trn [trn]
+  (jdbc/with-transaction [tx ds]
+    (doseq [item (:items trn)]
+      (if (contains? #{:POST :PUT} (:method item))
+        (->> (:body item)
+             (commit-boot-trn-item tx))
+        (throw (Exception. (str  "Method " (:method item) " not allowed while booting app")))))))
+
+(defn init []
+  (let [boot-file "./src/vamtyc/seeds/bootstrap.edn"]
+    (if (not (is-already-init?))
+      (-> (slurp boot-file)
+          (edn/read-string)
+          (commit-boot-trn)))))
+
+(comment
+  (init))
