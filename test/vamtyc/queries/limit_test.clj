bba39d75962e9bf3d2a81b6ffb6eb889f0990b0d
guillerglez88 <guillerglez88@gmail.com>
2022-12-11 18:37:48 +0100

    if _limit not found, env is used

--- test/vamtyc/queries/limit_test.clj
+++ test/vamtyc/queries/limit_test.clj
@@ -4,18 +4,25 @@
             [honey.sql :refer [format]]
             [honey.sql.helpers :refer [select from limit]]))
 
-(defn make-params []
-  {"_of"        "Resource"
-   "_limit"     "2"
-   "_type"      "List"
-   "_offset"    "0"
-   "_sort"      "_created"
-   "env/LIMIT" "128"})
+(defn make-params [keys]
+  (select-keys
+   {"_of"        "Resource"
+    "_limit"     "2"
+    "_type"      "List"
+    "_offset"    "0"
+    "_sort"      "_created"
+    "env/LIMIT"  "128"} keys))
 
 (defn make-sql []
   (-> (select :*)
       (from :Resource)))
 
 (t/deftest can-limit-results
-  (t/is (= ["SELECT * FROM Resource LIMIT ?" 2]
-           (format (sut/filter (make-sql) (make-params))))))
+  (t/testing "_limit param is choosen by default"
+    (t/is (= ["SELECT * FROM Resource LIMIT ?" 2]
+             (format (sut/filter (make-sql)
+                                 (make-params ["_env/LIMIT" "_limit"]))))))
+  (t/testing "if _limit not found, env is used"
+    (t/is (= ["SELECT * FROM Resource LIMIT ?" 128]
+             (format (sut/filter (make-sql)
+                                 (make-params ["env/LIMIT"])))))))
