760989f5f5ebc2371e997779dc15cc158ce1d8c8
guillerglez88 <guillerglez88@gmail.com>
2022-12-17 17:38:43 +0100

    total as a list prop

--- src/vamtyc/nerves/search.clj
+++ src/vamtyc/nerves/search.clj
@@ -10,28 +10,28 @@
             [honey.sql :as hsql]
             [lambdaisland.uri :refer [assoc-query uri-str]]))
 
+(defn nav-uri [url offset]
+  (-> url (assoc-query :_offset offset) uri-str))
+
 (defn result-set [req url total items]
   (let [offset  (-> req :vamtyc/queryp uqueryp/offset)
         limit   (-> req :vamtyc/queryp uqueryp/limit)
-        foffset 0
-        loffset (- total limit)
-        poffset (max foffset (- offset limit))
-        noffset (min loffset (+ offset limit))
-        first   (-> url (assoc-query :_offset foffset) uri-str)
-        prev    (-> url (assoc-query :_offset poffset) uri-str)
-        next    (-> url (assoc-query :_offset noffset) uri-str)
-        last    (-> url (assoc-query :_offset loffset :_total total) uri-str)]
-    {:type    :List
-     :url     url
-     :items   items
-     :nav     {:first first
-               :prev  prev
-               :next  next
-               :last  last}}))
+        first   0
+        last    (- total limit)
+        prev    (max first (- offset limit))
+        next    (min last (+ offset limit))]
+    {:type  :List
+     :url   url
+     :items items
+     :total total
+     :nav   {:first (nav-uri url first)
+             :prev  (nav-uri url prev)
+             :next  (nav-uri url next)
+             :last  (nav-uri url last)}}))
 
 (defn handler [req tx _app]
   (let [url     (relative-url req)
-        fields  (-> req :vamtyc/queryp uqueryp/fields (ufields/flat-expr))
+        fields  (-> req :vamtyc/queryp uqueryp/fields ufields/flat-expr)
         type    (-> req :vamtyc/route :path uroutes/type)
         of      (-> req :vamtyc/queryp uqueryp/of)
         sql-map (queries/search-query req tx)
